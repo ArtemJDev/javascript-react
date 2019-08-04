1236a24ee188a62772b6edc6610af732e1e5aff9
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-04 16:31:28 +0300

    projectJS

---
+++ src/main/java/javaScript/siteTest/js/script.js
@@ -0,0 +1,13 @@
+console.log("arr" + "tra"); //строка
+console.log("arr" + 5);  //строка
+
+// + перед строкой - в число
+
+var inc = 10;
+var decr = 10;
+
+// inc++;
+// decr--;
+
+console.log(++inc);
+console.log(--decr);
