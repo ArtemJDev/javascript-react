04434139723ed49cecc5e40154bd9839d9269f46
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-14 13:22:51 +0300

    Accumulator

---
+++ src/main/java/javaScript/siteTest/js/tsk/accumulator.js
@@ -0,0 +1,14 @@
+//запрашивает данные и суммирует с текущим значением.
+function Accumulator(startingValue) {
+    this.value = startingValue;
+  
+    this.read = function() {
+      this.value += +prompt('Сколько нужно добавить?', 0);
+    };
+  
+  }
+  
+  let accumulator = new Accumulator(1);
+  accumulator.read();
+  accumulator.read();
+  alert(accumulator.value);
