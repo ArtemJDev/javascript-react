8dd375b87cfde7b3432e46e435c6c5bf35a311c4
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-22 12:59:08 +0300

    tabsJS

---
+++ src/main/java/javaScript/siteTest/js/practice/tabs/script.js
@@ -0,0 +1,46 @@
+let tab ;
+let tabContent;
+
+window.onload =function() {
+    tabContent = document.querySelectorAll('.tabContent');
+    tab = document.querySelectorAll('.tab');
+
+    hideTabsContent(1);
+}
+//скрываем все кроме оджного
+function hideTabsContent(n) {
+    for( let i = n; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+        tab[i].classList.remove('whiteborder');
+        
+
+    }
+}
+
+function showsTabContent(n) {
+    console.log(tabContent[n]);
+
+    if(tabContent[n].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[n].classList.add('whiteborder');
+        tabContent[n].classList.remove('hide');
+        tabContent[n].classList.add('show');
+    }
+}
+
+document.getElementById('tabs').onclick = function(e) {
+    let target = e.target;
+    if(target.className == 'tab') {
+        for(let i = 0; i < tab.length; i ++) {
+            if( target == tab[i]) {
+  
+                console.log(e.target);
+
+                showsTabContent(i);
+                break;
+            }
+        }
+    }
+}
+
