3cb24a00ab215de044c195fa8819a4c898ca703c
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-24 17:05:03 +0300

    simpleAnimationJS

---
+++ src/main/java/javaScript/siteTest/js/practice/Animation/script.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector('#btn');
+
+function myMove() {
+    let elem = document.querySelector('#myAnimation');
+    let position = 0;
+    let id = setInterval(frame,10);
+    function frame() {
+        if( position == 350) {
+            clearInterval(id);
+        } else {
+            position++;
+            elem.style.top = position + 'px';
+            elem.style.left = position + 'px';
+
+        }
+    }
+}
+
+btn.addEventListener('click', function() {
+    myMove();
+});
