656c05319a156dda22eb9f45d08bb55791288168
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-02 13:03:26 +0300

    progressBar JS

---
+++ src/main/java/javaScript/siteTest/js/practice/progresBar/script.js
@@ -0,0 +1,20 @@
+function move() {
+    let elem = document.querySelector('#myBar');
+    let width = 10;
+    let id = setInterval(frame,100);
+    function frame() {
+        if(width>=100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width +'%';
+            document.querySelector('#label').innerHTML = width*1 + '%';
+        }
+
+    }
+}
+
+let btn = document.querySelector('.progress_btn');
+btn.addEventListener('click', ()=> {
+    move();
+});
