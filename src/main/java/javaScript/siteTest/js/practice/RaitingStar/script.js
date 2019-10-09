5f39fd2c25108d8ac9eac24f792b43d28e23ff1c
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-09 13:31:05 +0300

    RaitingStar refactor

--- src/main/java/javaScript/siteTest/js/practice/RaitingStar/script.js
+++ src/main/java/javaScript/siteTest/js/practice/RaitingStar/script.js
@@ -59,3 +59,17 @@ function mouseOutActiveClass(arr){
         }
     }
 }
+
+//refactor
+// (function(){
+// 	var container = document.querySelector('.rating');
+// 	var items = container.querySelectorAll('.rating-item')
+// 	container.onclick = function(e) {
+// 		if( ! e.target.classList.contains('active') ){
+// 			items.forEach(function(item){
+// 				item.classList.remove('active');
+// 			});
+// 			e.target.classList.add('active');
+// 		}
+// 	}
+// })();
