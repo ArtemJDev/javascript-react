d8b113b905d32c16f84796e055fed8cc6613c014
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-10 11:52:58 +0300

    preloader js

---
+++ src/main/java/javaScript/siteTest/js/practice/preloader/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+let preloader = document.getElementById('preloader');
+console.log("preloader")
+preloader.style.display = 'none';
+document.querySelector('.over').style.display = 'none';
+
+};
